971eded049e2cd6fa051fca17123b08c16e44af9
AmelieLockfat <116156925+AmelieLockfat@users.noreply.github.com>
2023-02-07 14:36:51 +0100

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <h1>Ma petite librairie</h1>
+  <h2>Bienvenue sur l'application librairie !</h2>
+  <img id="bibli" src="./imagebibliotheque.jpg" alt="bibliothèques" />
+  <img src="./background_colors.jpg" />
+</template>
+
+<script>
+</script>
+
+<style>
+#app {
+  font-family: Avenir, cursive, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+h1 {
+  color: lightcoral;
+  background-image: url("./background_colors.jpg") no repeat;
+}
+
+#bibli {
+  background-color: blueviolet;
+}
+
+h2 {
+  color: lightsalmon;
+}
+</style>
